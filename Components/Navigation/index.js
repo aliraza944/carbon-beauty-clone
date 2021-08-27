e6b8b08ccbd47d9ecbdda980312e4a27e5c9c5dd
Ali Raza <alisabri022@gmail.com>
2021-08-27 10:55:21 +0500

    build: theme.js

---
+++ Components/Navigation/index.js
@@ -0,0 +1,11 @@
+import { Typography } from "@material-ui/core";
+
+const Navigation = () => {
+  return (
+    <div>
+      <Typography variant="h1">naivgation</Typography>
+    </div>
+  );
+};
+
+export default Navigation;
--- src/theme.js
+++ src/theme.js
@@ -1,31 +1,69 @@
-import { createTheme } from "@material-ui/core/styles";
+import { createTheme, responsiveFontSizes } from "@material-ui/core";
+let theme = createTheme({
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 960,
+      lg: 1024,
+      xl: 1920,
+    },
+  },
+  props: {
+    MuiTypography: {
+      variantMapping: {
+        h1: "h1",
+        h2: "h2",
+        h3: "h3",
+        h4: "h4",
+        h5: "h5",
+        h6: "h6",
+        subtitle1: "h2",
+        subtitle2: "h2",
+        body1: "span",
+        body2: "span",
+      },
+    },
+  },
+  typography: {
+    fontFamily: ["Inter", "sans-serif"].join(","),
+  },
 
-// Create a theme instance.
-const theme = createTheme({
   overrides: {
     MuiCssBaseline: {
       "@global": {
         "*": {
-          // overflowX: "hidden",
+          padding: "0",
+          margin: "0",
           boxSizing: "border-box",
         },
         body: {
-          fontFamily: "Gorditas ,Helvetica Neue,sans-serif",
-          backgroundColor: "whitesmoke",
+          margin: "0",
+          color: "white",
         },
-        ".responsiveImage": {
-          width: "100% !important",
-          height: "auto important",
-          objectFit: "contain",
-          position: "relative !important",
-        },
-
-        a: {
+        ".link": {
           textDecoration: "none",
+          color: "white",
+        },
+        ".justifycenter": {
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        },
+        ".justifyspacebetween": {
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        },
+        ".justifyspacearound": {
+          display: "flex",
+          justifyContent: "space-around",
+          alignItems: "center",
         },
       },
     },
   },
 });
+theme = responsiveFontSizes(theme);
 
 export default theme;

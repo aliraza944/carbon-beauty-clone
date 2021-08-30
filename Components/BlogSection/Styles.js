d987b2390e2a556c46b9abb0220dd337d1223586
Ali Raza <alisabri022@gmail.com>
2021-08-30 21:08:31 +0500

    build: blog section created

--- Components/BlogSection/Styles.js
+++ Components/BlogSection/Styles.js
@@ -4,13 +4,9 @@ const useStyles = makeStyles((theme) => ({
     position: "relative",
     width: "100%",
     display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    padding: "8em",
+    flexDirection: "column",
 
-    "& .MuiTypography-root": {
-      color: "black",
-    },
+    padding: "8em",
   },
   inspiration: {
     display: "flex",
@@ -24,12 +20,65 @@ const useStyles = makeStyles((theme) => ({
       textTransform: "uppercase",
       letterSpacing: "2px",
       fontWeight: "500",
+      color: "black",
     },
   },
   theline: {
     width: "1px",
     height: "50px",
     backgroundColor: "black",
+  },
+  imgcontainer: {
+    width: "30%",
+    overflow: "hidden",
+    alignSelf: "center",
+    position: "relative",
+    backgroundColor: "blue",
+    display: "block",
+  },
+  blogimg: {
+    width: "100%",
+    objectFit: "cover",
+  },
+  toptext: {
+    position: "absolute",
+    fontWeight: "800",
+    zIndex: "1",
+  },
+  darktop: {
+    top: "50%",
+    left: "8%",
+
+    color: "black",
+    zIndex: -1,
+  },
+  lighttop: {
+    color: "white",
+    top: "37.7%",
+
+    left: "-117%",
+  },
+  darkbottom: {
+    top: "63.3%",
+    left: "85%",
+
+    color: "black",
+    zIndex: -1,
+  },
+  lightbottom: {
+    color: "white",
+    top: "60%",
+
+    left: "190%",
+  },
+  blogbtn: {
+    position: "absolute",
+    top: "50%",
+    right: "16%",
+    boxShadow: "none",
+    border: "2px solid black",
+    borderRadius: "0px",
+    backgroundColor: "transparent",
   },
 }));
 export default useStyles;
--- Components/BlogSection/index.js
+++ Components/BlogSection/index.js
@@ -1,6 +1,19 @@
-import { Typography } from "@material-ui/core";
+import Image from "next/image";
+import img1 from "../../public/Images/homeblog.jpg";
+import { Button, Typography } from "@material-ui/core";
+import useScroll from "../../Hooks/useScroll";
+import clsx from "clsx";
 import useStyles from "./Styles";
 const BlogSection = () => {
+  const scroll = useScroll();
+  const textStyle = {
+    transform: `translateX(${(scroll / 2200) * 75}%)`,
+    transition: `transform 0.5s linear`,
+  };
+  const textStyle2 = {
+    transform: `translateX(-${(scroll / 1900) * 75}%)`,
+    transition: `transform 0.5s linear`,
+  };
   const classes = useStyles();
   return (
     <div className={classes.root}>
@@ -8,6 +21,42 @@ const BlogSection = () => {
         <Typography variant="body2">Weekly inspiration</Typography>
         <div className={classes.theline}></div>
       </div>
+
+      <div className={classes.imgcontainer}>
+        <Image src={img1} layout="responsive" className={classes.blogimg} />
+        <Typography
+          variant="h2"
+          style={textStyle}
+          className={clsx(classes.toptext, classes.lighttop)}
+        >
+          Carbon
+        </Typography>
+        <Typography
+          variant="h2"
+          style={textStyle2}
+          className={clsx(classes.toptext, classes.lightbottom)}
+        >
+          Journey
+        </Typography>
+      </div>
+
+      <Typography
+        variant="h2"
+        style={textStyle}
+        className={clsx(classes.toptext, classes.darktop)}
+      >
+        Carbon
+      </Typography>
+      <Typography
+        variant="h2"
+        style={textStyle2}
+        className={clsx(classes.toptext, classes.darkbottom)}
+      >
+        Journey
+      </Typography>
+      <Button variant="contained" className={classes.blogbtn}>
+        Read the Blog
+      </Button>
     </div>
   );
 };
